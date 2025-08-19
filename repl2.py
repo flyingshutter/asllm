6487b52562ccff354eb2b845ee77f410d79a4cd1
flyingshutter <alois1.schneider@googlemail.com>
2025-08-20 01:41:55 +0200

    clean: remove unused imports

--- repl2.py
+++ repl2.py
@@ -3,11 +3,9 @@
 ################################################################################
 from prompt_toolkit import PromptSession
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.application import get_app 
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.formatted_text import HTML
 from prompt_toolkit.styles import Style      
-from prompt_toolkit.formatted_text import FormattedText as FT                                                                    
 from prompt_toolkit.completion import PathCompleter
 
 from rich.console import Console
